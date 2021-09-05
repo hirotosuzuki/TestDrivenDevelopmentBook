f34526e0b24a73692955ca1ca5ee9050509f6d56
hirotosuzuki <hiroto7665@gmail.com>
2021-09-06 02:31:28 +0900

    [Section:2]

--- test_driven_development/dollar.py
+++ test_driven_development/dollar.py
@@ -2,5 +2,5 @@ class Dollar:
     def __init__(self, amount:int):
         self.amount = amount
     
-    def times(self, multiplier:int) -> None:
-        self.amount *= multiplier
+    def times(self, multiplier:int):
+        return Dollar(self.amount * multiplier)
--- tests/test_test_driven_development.py
+++ tests/test_test_driven_development.py
@@ -9,5 +9,7 @@ def test_version():
 class TestMoney:
     def test_multiplication(self):
         five = Dollar(5)
-        five.times(2)
-        assert five.amount==10
+        product = five.times(2)
+        assert product.amount==10
+        product = five.times(3)
+        assert product.amount==15
